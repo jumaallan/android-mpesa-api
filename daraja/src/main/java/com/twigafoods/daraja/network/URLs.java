94da942516b2033dcb09015add9728599f5cf63d
Juma Allan <allanjuma@gmail.com>
2018-04-13 23:34:29 +0300

    Added URL Endpoints

---
+++ daraja/src/main/java/com/twigafoods/daraja/network/URLs.java
@@ -0,0 +1,9 @@
+package com.twigafoods.daraja.network;
+
+public class URLs {
+    public static final String SANDBOX_BASE_URL = "https://sandbox.safaricom.co.ke/";
+    public static final String PRODUCTION_BASE_URL = "https://api.safaricom.co.ke/";
+
+    public static final String ACCESS_TOKEN_URL = "oauth/v1/generate?grant_type=client_credentials";
+    public static final String PROCESS_REQUEST_URL ="mpesa/stkpush/v1/processrequest";
+}
