758803a12be6bd319788c401b55215f53f8f504b
Juma Allan <allanjuma@gmail.com>
2018-04-16 13:11:28 +0300

    Updated LNMExpress Model

--- app/src/main/java/com/twigafoods/mpesa/MainActivity.java
+++ app/src/main/java/com/twigafoods/mpesa/MainActivity.java
@@ -3,6 +3,7 @@ package com.twigafoods.mpesa;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
@@ -48,31 +49,43 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //TODO :: REPLACE WITH YOUR OWN CREDENTIALS  :: THIS IS SANDBOX DEMO
-        LNMExpress lnmExpress = new LNMExpress(
-                "600195",
-                "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919",  //https://developer.safaricom.co.ke/test_credentials
-                "100",
-                "254708374149",
-                "174379",
-                phoneNumber,
-                "http://mycallbackurl.com/mpesa",
-                "0001",
-                "Goods Payment"
-        );
+        //TODO :: THIS IS A SIMPLE WAY TO DO ALL THINGS AT ONCE!!! DON'T DO THIS :)
+        sendButton.setOnClickListener(v -> {
 
-        sendButton.setOnClickListener(v -> daraja.sendSTKPush(lnmExpress,
-                new DarajaListener<LNMResult>() {
-                    @Override
-                    public void onResult(@NonNull LNMResult lnmResult) {
-                        Log.i(MainActivity.this.getClass().getSimpleName(), lnmResult.ResponseDescription);
-                    }
+            //Get Phone Number from User Input
+            phoneNumber = editTextPhoneNumber.getText().toString().trim();
 
-                    @Override
-                    public void onError(String error) {
-                        Log.i(MainActivity.this.getClass().getSimpleName(), error);
+            if (TextUtils.isEmpty(phoneNumber)) {
+                editTextPhoneNumber.setError("Please Provide a Phone Number");
+                return;
+            }
+
+            //TODO :: REPLACE WITH YOUR OWN CREDENTIALS  :: THIS IS SANDBOX DEMO
+            LNMExpress lnmExpress = new LNMExpress(
+                    "600195",
+                    "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919",  //https://developer.safaricom.co.ke/test_credentials
+                    "100",
+                    "254708374149",
+                    "174379",
+                    phoneNumber,
+                    "http://mycallbackurl.com/mpesa",
+                    "0001",
+                    "Goods Payment"
+            );
+
+            daraja.sendSTKPush(lnmExpress,
+                    new DarajaListener<LNMResult>() {
+                        @Override
+                        public void onResult(@NonNull LNMResult lnmResult) {
+                            Log.i(MainActivity.this.getClass().getSimpleName(), lnmResult.ResponseDescription);
+                        }
+
+                        @Override
+                        public void onError(String error) {
+                            Log.i(MainActivity.this.getClass().getSimpleName(), error);
+                        }
                     }
-                }
-        ));
+            );
+        });
     }
 }
