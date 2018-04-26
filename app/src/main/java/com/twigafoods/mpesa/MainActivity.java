eb9bef5eda38d3f9c9778025a8d020fd85d57f63
Juma Allan <allanjuma@gmail.com>
2018-04-26 12:46:54 +0300

    Updating Example

--- app/src/main/java/com/twigafoods/mpesa/MainActivity.java
+++ app/src/main/java/com/twigafoods/mpesa/MainActivity.java
@@ -5,8 +5,6 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -35,9 +33,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
---
+++ app/src/main/java/com/twigafoods/mpesa/mpesaexpress/MPESAExpressActivity.java
@@ -0,0 +1,15 @@
+package com.twigafoods.mpesa.mpesaexpress;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.twigafoods.mpesa.R;
+
+public class MPESAExpressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mpesaexpress);
+    }
+}
