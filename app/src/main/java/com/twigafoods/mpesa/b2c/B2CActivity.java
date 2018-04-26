f6fb6e58ad1ad9698454492cf6ece68ae65161e0
Juma Allan <allanjuma@gmail.com>
2018-04-26 12:50:06 +0300

    Added B2C Demo Activity

---
+++ app/src/main/java/com/twigafoods/mpesa/b2c/B2CActivity.java
@@ -0,0 +1,13 @@
+package com.twigafoods.mpesa.b2c;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class B2CActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b2c);
+    }
+}
