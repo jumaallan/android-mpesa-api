8ccaa223d79c265f8ea06b57900abaf7da549a92
Juma Allan <allanjuma@gmail.com>
2018-04-26 12:51:00 +0300

    Added B2B Demo Activity

--- app/src/main/java/com/twigafoods/mpesa/b2b/B2BActivity.java
+++ app/src/main/java/com/twigafoods/mpesa/b2b/B2BActivity.java
@@ -1,15 +1,15 @@
-package com.twigafoods.mpesa.b2c;
+package com.twigafoods.mpesa.b2b;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 import com.twigafoods.mpesa.R;
 
-public class B2CActivity extends AppCompatActivity {
+public class B2BActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_b2c);
+        setContentView(R.layout.activity_b2b);
     }
 }
